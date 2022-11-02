5117c5a313ec7fe7f71b0b281415dca45bf28377
Raczynska <anna.raczynska@capgemini.com>
2022-11-02 16:35:05 +0100

    routes

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const trips = require('../repos/repoFunctions');
+const router = express.Router();
+
+
+router.get('/trips', trips.get)
+router.get('/trip/:id', trips.getById)
+router.post('/trip', trips.createTrip)
+router.put('/trip/:id', trips.updateTrip)
+router.delete('trip/:id', trips.deleteTrip)
+
+
+module.exports = (router)
